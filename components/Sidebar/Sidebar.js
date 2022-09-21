0db420cb679f82685834b164a50375194c5f5db4
Jahid Hasan <jahidhasan2856@gmail.com>
2022-09-21 15:31:17 +0600

    sidebar completed

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -5,18 +5,26 @@ import OutsideClickHandler from "react-outside-click-handler";
 const menus = [
   { id: 1, name: "Work", url: "/work" },
   { id: 2, name: "What we do", url: "/we-do" },
-  { id: 3, name: "Blogs", url: "/blogs" },
   { id: 4, name: "Lets Connect", url: "/connect" },
 ];
+const moreMenu = [
+  { id: 1, name: "About Us", url: "/about-us" },
+  { id: 2, name: "Work with us", url: "/work-with-us" },
+  { id: 3, name: "Blogs", url: "/blogs" },
+];
+
 export default function Sidebar({ sidebar, setSidebar }) {
   return (
     <aside
       className="fixed h-screen top-0 left-0 right-0
-       bg-[#0000009c]
-     w-screen z-30 flex justify-end"
+       
+     w- z-30 flex justify-end"
     >
       <OutsideClickHandler onOutsideClick={() => setSidebar(false)}>
-        <div className={`${sidebar ? "w-[300px]": 'w-0px'} transition-all bg-[#00588A] h-screen p-5 mr-[25px]`}>
+        <div
+          className={` w-[538px] bg-[#00588AB2] backdrop-blur-[27.1828px]
+           overflow-hidden h-screen px-[60px] pt-[57px] relative`}
+        >
           <div className="flex justify-end">
             <svg
               onClick={() => setSidebar(false)}
@@ -25,7 +33,7 @@ export default function Sidebar({ sidebar, setSidebar }) {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-12 h-12 cursor-pointer p-2 text-white hover:text-[#243479]
+              className="w-12 h-12 cursor-pointer  text-white hover:text-[#243479]
                rounded-full hover:bg-white transition-all duration-300"
             >
               <path
@@ -37,12 +45,11 @@ export default function Sidebar({ sidebar, setSidebar }) {
           </div>
           <ul className="">
             {menus.map((menu) => (
-              <li key={menu.id} className="my-10">
+              <li key={menu.id} className="mb-[70px]">
                 <Link href={menu.url}>
                   <a
-                    className="font-semibold text-[20px]
-                        leading-7 text-white rounded-md
-                   px-4 py-3 w-full block hover:bg-[#0180bc2b] transition-all duration-200 "
+                    className="font-semibold text-[24px]
+                    leading-7 text-white rounded-md px-4 py-3 "
                   >
                     {menu.name}
                   </a>
@@ -50,6 +57,38 @@ export default function Sidebar({ sidebar, setSidebar }) {
               </li>
             ))}
           </ul>
+          <hr className="borderr border-[#F2F2F2]" />
+
+          <ul className="">
+            {moreMenu.map((menu) => (
+              <li key={menu.id} className="my-[40px]">
+                <Link href={menu.url}>
+                  <a
+                    className="font-semibold text-[16px]
+                        leading-5 text-white rounded-md
+                   px-4 py-3"
+                  >
+                    {menu.name}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="absolute bottom-0 w-full">
+            <div className="flex justify-between w-full relative">
+              <img src="/assets/sidebar-send.svg" alt="bottom send" />
+              <img
+                className="top-2/4 left-[30%] absolute"
+                src="/assets/sidebar-send.svg"
+                alt="bottom send"
+              />
+              <img
+                className="absolute right-[30%] w-16 bottom-0"
+                src="/assets/sidebar-send.svg"
+                alt="bottom send"
+              />
+            </div>
+          </div>
         </div>
       </OutsideClickHandler>
     </aside>
